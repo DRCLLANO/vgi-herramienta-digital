028772d309aa58e44d315d8b5f4329fa4f0b1a90
DRCLLANO <drnexusgeri@gmail.com>
2026-09-14 01:08:58 -0500

    Update sw.js
    ajuste en versión

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@
    Al cambiar de versión se cambia CACHE y se borran las cachés viejas.
    ======================================================================= */
 
-const CACHE = 'escalas-geriatria-v2.10.4';
+const CACHE = 'escalas-geriatria-v2.10.5';
 
 const PRECARGA = [
   './',
